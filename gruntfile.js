154f0f9baf3c6dea3285f6cc203c08575bda726d
DigbijayNayak555 <digbijay.nayak@peopletech.com>
2024-11-05 11:45:51 +0530

    integrating bower into our workflow

--- gruntfile.js
+++ gruntfile.js
@@ -32,6 +32,12 @@ module.exports = function (grunt) {
       },
     }, //sass
 
+    wiredep: {
+      task: {
+        src: 'builds/development/**/*.html',
+      },
+    },
+
     connect: {
       server: {
         options: {
@@ -63,6 +69,13 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-wiredep');
 
-  grunt.registerTask('default', ['concat', 'sass', 'connect', 'watch']);
+  grunt.registerTask('default', [
+    'wiredep',
+    'concat',
+    'sass',
+    'connect',
+    'watch',
+  ]);
 }; //wrapper function
