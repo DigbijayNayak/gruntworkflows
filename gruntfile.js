2d94cb0fb584a24a4ae959d2352c19838df7e5c8
DigbijayNayak555 <digbijay.nayak@peopletech.com>
2024-11-05 09:40:48 +0530

    processing sass files

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,5 @@
+const sass = require('node-sass');
+
 module.exports = function (grunt) {
   grunt.initConfig({
     concat: {
@@ -13,9 +15,26 @@ module.exports = function (grunt) {
         src: ['components/scripts/*.js'],
         dest: 'builds/production/js/script.js',
       },
-    },
+    }, //concat
+
+    sass: {
+      dist: {
+        options: {
+          implementation: sass,
+          style: 'expanded',
+        },
+        files: [
+          {
+            src: 'components/sass/style.scss',
+            dest: 'builds/development/css/style.css',
+          },
+        ],
+      },
+    }, //sass
   }); //initConfig
 
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.registerTask('default', ['concat']);
+  grunt.loadNpmTasks('grunt-sass');
+
+  grunt.registerTask('default', ['concat', 'sass']);
 }; //wrapper function
