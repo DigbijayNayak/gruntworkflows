619987b13b9d75edd1335000ebbca795695a6119
DigbijayNayak555 <digbijay.nayak@peopletech.com>
2024-11-05 09:04:21 +0530

    adding task options and defaults

--- gruntfile.js
+++ gruntfile.js
@@ -1,12 +1,21 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     concat: {
+      options: {
+        separator: '\n\n//---------------------------------------------\n',
+        banner: '\n\n//------------------------------------------------\n',
+      },
       dist: {
         src: ['components/scripts/*.js'],
         dest: 'builds/development/js/script.js',
+      },
+      prod: {
+        src: ['components/scripts/*.js'],
+        dest: 'builds/production/js/script.js',
       },
     },
   }); //initConfig
 
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('default', ['concat']);
 }; //wrapper function
